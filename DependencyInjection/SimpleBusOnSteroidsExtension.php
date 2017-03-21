6c85f91663095fcfbb19b653e49dfaa356f9c2e9
Dariusz Gafka <dgafka.mail@gmail.com>
2017-03-21 15:31:35 +0100

    configuration

--- DependencyInjection/SimpleBusOnSteroidsExtension.php
+++ DependencyInjection/SimpleBusOnSteroidsExtension.php
@@ -5,7 +5,7 @@ namespace CleanCode\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * Class SimpleBusOnSteroidsExtension
@@ -19,10 +19,21 @@ class SimpleBusOnSteroidsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $container->setParameter('empty_array', []);
 
-        $loader->load('asynchronous_steroids_events.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
+        $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('simple_bus_max_requeue_times', $config['simple_bus_max_requeue_times']);
+        $container->setParameter('simple_bus_base_time_in_seconds', $config['simple_bus_base_time_in_seconds']);
+        $container->setParameter('simple_bus_requeue_multiply_time_by', $config['simple_bus_requeue_multiply_time_by']);
+        $container->setParameter('simple_bus_synchronize_for_message_amount', $config['simple_bus_synchronize_for_message_amount']);
+        $container->setParameter('simple_bus_how_many_events_at_once', $config['simple_bus_how_many_events_at_once']);
+        $container->setParameter('simple_bus_send_messages_every_seconds', $config['simple_bus_send_messages_every_seconds']);
+        $container->setParameter('simple_bus_dead_letter_exchange_name', $config['simple_bus_dead_letter_exchange_name']);
+        $container->setParameter('simple_bus_dead_letter_queue_name', $config['simple_bus_dead_letter_queue_name']);
     }
 }
